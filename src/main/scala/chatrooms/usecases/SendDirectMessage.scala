5b0f4be3e112fe9b54aa112d419dee5acbff1c05
frabbit <frabbit77@googlemail.com>
2022-07-11 19:45:53 +0200

    split SendDirectMessage module interface and Live implementation

--- src/main/scala/chatrooms/usecases/SendDirectMessage.scala
+++ src/main/scala/chatrooms/usecases/SendDirectMessage.scala
@@ -12,20 +12,7 @@ trait SendDirectMessage {
   def sendDirectMessage(from:ClientId, to:UserName, msg:String):ZIO[Any, Nothing, Unit]
 }
 
-final case class SendDirectMessageLive(stateRef:TRef[ServerState], server:SocketServer) extends SendDirectMessage:
-  def sendDirectMessage(from:ClientId, to: UserName, msg: String):ZIO[Any, Nothing, Unit] =
-    for {
-        state <- stateRef.get.commit
-        receiverClientId = state.clients.find(_._2.name == to).map(_._2.id)
-        fromName = state.clients.find(_._2.id == from).map(_._2.name)
-        _ <- zio.Console.printLine("SendDirectMessage " ++ (receiverClientId, fromName).toString).ignore
-        _ <- (fromName, receiverClientId).match {
-          case (Some(f), Some(id)) => server.sendTo(id, ServerMessage.DirectMessage(f, msg).encode).ignore
-          case _ => ZIO.unit
-        }
-      } yield ()
-
-
-object SendDirectMessageLive:
-  val layer:ZLayer[TRef[ServerState] & SocketServer, Nothing, SendDirectMessage] = ZLayer.fromFunction(SendDirectMessageLive.apply)
-
+object SendDirectMessage {
+  def sendDirectMessage(from:ClientId, to:UserName, msg:String):ZIO[SendDirectMessage, Nothing, Unit] =
+    ZIO.serviceWith[SendDirectMessage](_.sendDirectMessage(from, to, msg))
+}
---
+++ src/main/scala/chatrooms/usecases/SendDirectMessageLive.scala
@@ -0,0 +1,26 @@
+package chatrooms.usecases
+
+import zio.*
+import zio.stm.TRef
+import chatrooms.domain.ServerState
+import chatrooms.domain.SocketServer
+import chatrooms.domain.ClientId
+import chatrooms.domain.UserName
+import chatrooms.domain.ServerMessage
+
+final case class SendDirectMessageLive(stateRef:TRef[ServerState], server:SocketServer) extends SendDirectMessage:
+  def sendDirectMessage(from:ClientId, to: UserName, msg: String):ZIO[Any, Nothing, Unit] =
+    for {
+        state <- stateRef.get.commit
+        receiverClientId = state.clients.find(_._2.name == to).map(_._2.id)
+        fromName = state.clients.find(_._2.id == from).map(_._2.name)
+        _ <- zio.Console.printLine("SendDirectMessage " ++ (receiverClientId, fromName).toString).ignore
+        _ <- (fromName, receiverClientId).match {
+          case (Some(f), Some(id)) => server.sendTo(id, ServerMessage.DirectMessage(f, msg).encode).ignore
+          case _ => ZIO.unit
+        }
+      } yield ()
+
+object SendDirectMessageLive:
+  val layer:ZLayer[TRef[ServerState] & SocketServer, Nothing, SendDirectMessage] = ZLayer.fromFunction(SendDirectMessageLive.apply)
+
