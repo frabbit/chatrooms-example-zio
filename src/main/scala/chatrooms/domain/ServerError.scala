5ec25ab45db821a430d4ea01a696dadca611ac0b
frabbit <frabbit77@googlemail.com>
2022-07-13 20:58:24 +0200

    move ServerError into separate file

---
+++ src/main/scala/chatrooms/domain/ServerError.scala
@@ -0,0 +1,61 @@
+package chatrooms.domain
+
+import java.util.UUID
+import parsley.Parsley, Parsley._
+import parsley.combinator as C
+import parsley.combinator.{many, some, eof, option}
+import parsley.character.{char, string, digit, oneOf, noneOf}
+import parsley.character as Character
+import parsley.implicits.character.{charLift, stringLift}
+
+sealed trait ServerError {
+  def encode = ServerErrorEncoder.encode(this)
+}
+
+object ServerErrorEncoder {
+  def encode (c:ServerError):String = c.match {
+    case ServerError.UserIsNotInRoom => "userIsNotInRoom"
+    case ServerError.AlreadyJoined => "alreadyJoined"
+    case ServerError.UserNameTaken => "userNameTaken"
+    case ServerError.RoomNotFound(room) => "roomNotFound " ++ room.encode
+  }
+}
+
+object ServerErrorParser {
+  val alreadyJoinedParser: Parsley[ServerError.AlreadyJoined] = for {
+    _ <- attempt(string("alreadyJoined"))
+    c <- Parsley.pure(ServerError.AlreadyJoined)
+  } yield c
+
+  val userNameTakenParser: Parsley[ServerError.UserNameTaken] = for {
+    _ <- attempt(string("userNameTaken"))
+    c <- Parsley.pure(ServerError.UserNameTaken)
+  } yield c
+
+  val userIsNotInRoomParser: Parsley[ServerError.UserIsNotInRoom] = for {
+    _ <- attempt(string("userIsNotInRoom"))
+    c <- Parsley.pure(ServerError.UserIsNotInRoom)
+  } yield c
+
+  val roomNotFoundParser: Parsley[ServerError.RoomNotFound] = for {
+    _ <- attempt(string("roomNotFound"))
+    _ <- char(' ')
+    roomName <- RoomName.parser
+    c <- Parsley.pure(ServerError.RoomNotFound(roomName))
+  } yield c
+
+  val parser: Parsley[ServerError] = for {
+    c <- alreadyJoinedParser <|> userNameTakenParser <|> roomNotFoundParser <|> userIsNotInRoomParser
+    _ <- eof
+  } yield c
+}
+
+object ServerError {
+  case object AlreadyJoined extends ServerError
+  type AlreadyJoined = AlreadyJoined.type
+  case object UserNameTaken extends ServerError
+  type UserNameTaken = UserNameTaken.type
+  case class RoomNotFound(room:RoomName) extends ServerError
+  case object UserIsNotInRoom extends ServerError
+  type UserIsNotInRoom = UserIsNotInRoom.type
+}
